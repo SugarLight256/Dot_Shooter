7a4b0364089bc5886961c0f3478bdab912c6c094
SugarLight256 <nippen12@gmail.com>
2016-10-16 17:01:00 +0900

    first commit: Edit Scene

---
+++ Assets/Script/Managers/Block.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BlockID{
+    None,
+    Core,
+    Common,
+    Thruster,
+    Shield,
+
+}
+
+public class Block{
+
+    private BlockID knd;
+
+    Block()
+    {
+        knd = BlockID.None;
+    }
+
+
+    public void setKnd(BlockID knd)
+    {
+        this.knd = knd;
+    }
+
+    public void setColor(Color color)
+    {
+        if(knd == BlockID.Common)
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Script/Managers/BlockMapManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMapManager : MonoBehaviour {
+
+    public static Block[,] BlockMap = new Block[15,15];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void setBlock(int x, int y, BlockID knd)
+    {
+        BlockMap[x, y].setKnd(knd);
+    }
+
+    public void setColor(int x, int y, Color color)
+    {
+        BlockMap[x, y].setColor(color);
+    }
+}
